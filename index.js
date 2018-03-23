c7de5ed0a8d92eeab92efd51fe90a2dd927ac823
Ito Yoshiaki <ito-yoshiaki@skw.canon-soft.co.jp>
2018-03-23 18:07:45 +0900

    iroiro

--- index.js
+++ index.js
@@ -2,10 +2,42 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+const LINE_CHANNEL_ACCESS_TOKEN = 'xxxxxxxxxxxxxxx';
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
+
+function reply(event, text){
+    var headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+    }
+    console.log(headers);
+    var body = {
+        replyToken: event.replyToken,
+        messages: [{
+            type: 'text',
+            text: text
+        }]
+    }
+    console.log(body);
+    var url = 'https://api.line.me/v2/bot/message/reply';
+    request({
+        url: url,
+        method: 'POST',
+        headers: headers,
+        body: body,
+        json: true
+    }, function(error, response, bdy){
+        if (!error && response.statusCode == 200) {
+            console.log(bdy);
+        } else {
+            console.log('error: '+ response.statusCode);
+        }
+    });
+}
 
 app.post('/', function(request, response) {
   console.log('post');
@@ -14,6 +46,39 @@ app.post('/', function(request, response) {
   console.log(request.body.events[0].beacon.dm);
   console.log(request.body.events[0].beacon.type);
   response.sendStatus(200);
+  for (var event of request.body.events){
+    console.log('event.type : ' + event.type);
+    console.log('event.source.type : ' + event.source.type);
+    if (event.source.type == 'user') {
+      console.log('event.source.userId : ' + event.source.userId);
+    }
+    if (event.type == 'message') {
+        if (event.message.text == 'id' || event.message.text == 'ID' || event.message.text == 'ÇâÇÑ' || event.message.text == 'ÇhÇc'){
+            reply(event, 'Ç†Ç»ÇΩÇÃIDÇÕÅu' + event.source.userId + 'ÅvÇ≈Ç∑');
+        } else {
+            hash_list[event.source.userId]  = event.message.text;
+        }
+    } else if (event.type == 'beacon') {
+        var dt = new Date(event.timestamp);
+        dt.setTime(dt.getTime() + 32400000); // 1000 * 60 * 60 * 9(hour)
+        var month = dt.getMonth()+1;
+        var day   = dt.getDate();
+        var hour  = dt.getHours();
+        var min   = dt.getMinutes();
+        if (min   < 10) {
+            min   = '0' + min;
+        }
+        console.log(month + '/' + day + ' ' + hour + ':' + min);
+        console.log('event.beacon.type : ' + event.beacon.type);
+        console.log('event.beacon.hwid : ' + event.beacon.hwid);
+        reply(event, month + '/' + day + ' ' + hour + ':' + min + ' ' + event.beacon.type);
+    }
+    if (event.type == 'postback'){
+      console.log('postback: '+ event.postback.data);
+      hash_list[event.source.userId]  = event.postback.data;
+    }
+  }
+
 });
 
 app.listen(app.get('port'), function() {
--- linebeacon/linebeacon.ino
+++ linebeacon/linebeacon.ino
@@ -3,27 +3,27 @@
 
 BLEDevice ble;
 
+uint8_t sid[] = {0x6F, 0xFE};
+uint8_t sdata[] = {0x6F, 0xFE, 0x02, 0x01, 0xDE, 0xAD, 0xBE, 0xEF, 0x7F, 0x00};
+uint8_t hwid[] = HWID;
+
 void setup() {
   Serial.begin(9600);
-  uint8_t da[] = {0x6F, 0xFE};
-  uint8_t data[] = {0x6F, 0xFE, 0x02, 0x01, 0xDE, 0xAD, 0xBE, 0xEF, 0x7F, 0x00};
-//  uint8_t hwid[] = {0x01, 0x0e, 0xd7, 0x8c, 0xd3};
-  uint8_t hwid[] = HWID;
 
   // put your setup code here, to run once
   // close peripheral power
   NRF_POWER->DCDCEN = 0x00000001;
-  
+
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
-  
+
+  memcpy(&sdata[3], hwid, 5);
+
   ble.init(); 
   // set advertisement
   ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
-//  ble.accumulateAdvertisingPayload(GapAdvertisingData::MANUFACTURER_SPECIFIC_DATA, beaconPayload, sizeof(beaconPayload));
-  ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS , da, sizeof(da));
-  ble.accumulateAdvertisingPayload(GapAdvertisingData::SERVICE_DATA , data, sizeof(data));
-//  ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, uuid16_list, sizeof(uuid16_list));
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS , sid, sizeof(sid));
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::SERVICE_DATA , sdata, sizeof(sdata));
   // set advertise type  
   //  ADV_CONNECTABLE_UNDIRECTED
   //  ADV_CONNECTABLE_DIRECTED
